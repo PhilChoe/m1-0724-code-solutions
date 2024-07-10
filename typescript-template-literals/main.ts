09f38380f12a64a2e10187b128ed1fd384b82fe3
Philip J Choe <j12choe@gmail.com>
2024-07-10 03:23:02 +0000

    Completed typescript-template-literals clear

---
+++ typescript-template-literals/main.ts
@@ -0,0 +1,26 @@
+const number = 9200;
+const street = 'Irvine Center Drive';
+// const address = number + ' ' + street;
+const interpolatedAddress = `LearningFuze: ${number} ${street}`;
+
+console.log(interpolatedAddress);
+
+const firstName = 'Uzair';
+const lastName = 'Ashraf';
+const age = 25;
+const interpolatedBio = `Bio: My Name is ${firstName} ${lastName} and I am ${age} years old`;
+
+// const bio =
+// 'My name is ' + firstName + ' ' + lastName + ' and I am ' + age + ' years old.';
+
+console.log(interpolatedBio);
+
+const fruit = 'apple';
+const item = 'pen';
+
+const applePen = fruit + item;
+
+const interpolatedApplePen = `I have a ${item}
+I have an ${fruit}
+${applePen}`;
+console.log(interpolatedApplePen);
