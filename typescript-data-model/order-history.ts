e6c13c0401de734dfce8146e81c1464cf92a1ce9
Philip J Choe <j12choe@gmail.com>
2024-07-12 05:20:57 +0000

    Completed typescript-data-model

---
+++ typescript-data-model/order-history.ts
@@ -0,0 +1,46 @@
+interface OrderHistory {
+  title?: string;
+  author?: string;
+  returnWindow?: string;
+  price?: number;
+  product?: string;
+}
+
+const orderHistory: OrderHistory[] = [
+  {
+    title: 'Javascript for impatient programmers',
+    author: 'Rauschmayer, Dr. Axel',
+    returnWindow: 'Return window closed on Sep 7, 2020',
+    price: 31.55,
+  },
+  {
+    title: 'The Timeless Way of Building',
+    author: 'Alexander, Christopher',
+    returnWindow: 'Return window closed on Aug 19, 2020',
+    price: 41.33,
+  },
+  {
+    title: '',
+    author: 'Rauschmayer, Dr. Axel',
+    returnWindow: 'Return window closed on Sep 7, 2020',
+    price: 31.55,
+  },
+  {
+    product:
+      'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    returnWindow: 'Return window closed on Aug 5, 2020',
+    price: 15.98,
+  },
+  {
+    product:
+      'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+    returnWindow: 'Return window closed on Aug 4, 2020',
+    price: 94.95,
+  },
+  {
+    title: 'The Art of Sql',
+    author: 'Faroult, Stephanie',
+    returnWindow: 'Return window closed on Aug 4, 2020',
+    price: 33.99,
+  },
+];
