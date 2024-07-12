ba8c0f3fe3de9c06fbbf3e91b85568739fe47ffd
Philip J Choe <j12choe@gmail.com>
2024-07-12 23:50:00 +0000

    Re-written data model

--- typescript-data-model/order-history.ts
+++ typescript-data-model/order-history.ts
@@ -1,46 +1,83 @@
-interface OrderHistory {
-  title?: string;
-  author?: string;
-  returnWindow?: string;
-  price?: number;
-  product?: string;
+interface Product {
+  bookTitle?: string;
+  item?: string;
+  bookAuthor?: string;
+  bookReturnWindow?: string;
+  itemReturnWindow?: string;
+  bookPrice?: number;
+  itemPrice?: number;
 }
 
-const orderHistory: OrderHistory[] = [
+interface Order {
+  orderPlaced: string;
+  total: number;
+  shipTo: string;
+  orderNumber: string;
+  deliveredDate: string;
+  product: Product[];
+}
+
+const order: Order[] = [
   {
-    title: 'Javascript for impatient programmers',
-    author: 'Rauschmayer, Dr. Axel',
-    returnWindow: 'Return window closed on Sep 7, 2020',
-    price: 31.55,
+    orderPlaced: 'August 4, 2020',
+    total: 34.0,
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    deliveredDate: 'Aug 8, 2020',
+    product: [
+      {
+        bookTitle: 'Javascript for impatient programmers',
+        bookAuthor: 'Rauschmayer, Dr. Axel',
+        bookReturnWindow: 'Return Window closed on Sep 7, 2020',
+        bookPrice: 31.55,
+      },
+    ],
   },
   {
-    title: 'The Timeless Way of Building',
-    author: 'Alexander, Christopher',
-    returnWindow: 'Return window closed on Aug 19, 2020',
-    price: 41.33,
+    orderPlaced: 'July 19, 2020',
+    total: 44.53,
+    shipTo: 'JS Masher',
+    orderNumber: '113-9984268-1280257',
+    deliveredDate: 'July 20, 2020',
+    product: [
+      {
+        bookTitle: 'The Timeless Way of Building',
+        bookAuthor: 'Alexander, Christopher',
+        bookReturnWindow: 'Return Window closed on Aug 19, 2020',
+        bookPrice: 41.33,
+      },
+    ],
   },
   {
-    title: '',
-    author: 'Rauschmayer, Dr. Axel',
-    returnWindow: 'Return window closed on Sep 7, 2020',
-    price: 31.55,
+    orderPlaced: 'July 4, 2020',
+    total: 17.22,
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    deliveredDate: 'July 7, 2020',
+    product: [
+      {
+        item: 'Gamecube Contoller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        itemReturnWindow: 'Return Window closed on Aug 5, 2020',
+        itemPrice: 15.98,
+      },
+    ],
   },
   {
-    product:
-      'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
-    returnWindow: 'Return window closed on Aug 5, 2020',
-    price: 15.98,
-  },
-  {
-    product:
-      'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
-    returnWindow: 'Return window closed on Aug 4, 2020',
-    price: 94.95,
-  },
-  {
-    title: 'The Art of Sql',
-    author: 'Faroult, Stephanie',
-    returnWindow: 'Return window closed on Aug 4, 2020',
-    price: 33.99,
+    orderPlaced: 'July 3, 2020',
+    total: 138.93,
+    shipTo: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    deliveredDate: 'July 5, 2020',
+    product: [
+      {
+        item: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        itemReturnWindow: 'Return Window closed on Aug 4, 2020',
+        itemPrice: 94.95,
+        bookTitle: 'The Art of Sql',
+        bookAuthor: 'Faroult, Stephane',
+        bookReturnWindow: 'Return Window closed on Aug 4, 2020',
+        bookPrice: 33.99,
+      },
+    ],
   },
 ];
