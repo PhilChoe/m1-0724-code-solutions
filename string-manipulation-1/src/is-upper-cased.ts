193019dd16fdcbcd42a0ced659b62c4f269dec4b
Philip J Choe <j12choe@gmail.com>
2024-07-27 20:53:22 +0000

    changed code for upper cased

--- string-manipulation-1/src/is-upper-cased.ts
+++ string-manipulation-1/src/is-upper-cased.ts
@@ -1,14 +1,5 @@
 /* exported isUpperCased */
 
 function isUpperCased(word: string): boolean {
-  for (let i = 0; i < word.length; i++) {
-    if (word === word.toUpperCase()) return true;
-  }
-  return false;
-}
-
-/*
-function isUpperCased(word): boolean {
   return word === word.toUpperCase();
 }
-*/
