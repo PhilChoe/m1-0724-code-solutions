288dbddaeb09ee1cd6f21ec4cea65964f1a70512
Philip J Choe <j12choe@gmail.com>
2024-08-21 06:32:35 +0000

    completed dom-manipulation

---
+++ dom-manipulation/main.ts
@@ -0,0 +1,32 @@
+let $count = 0;
+
+const $button = document.querySelector('.hot-button');
+const $click = document.querySelector('.click-count');
+
+if (!$button) throw new Error('The $button query failed');
+
+function handleClick(): void {
+  $count++;
+
+  if (!$button || !$click) {
+    throw new Error('The $button or $click query failed');
+  }
+
+  $click.textContent = `Click Count: ${$count}`;
+
+  if ($count < 4) {
+    $button.className = 'cold';
+  } else if ($count < 7) {
+    $button.className = 'cool';
+  } else if ($count < 10) {
+    $button.className = 'tepid';
+  } else if ($count < 13) {
+    $button.className = 'warm';
+  } else if ($count < 16) {
+    $button.className = 'hot';
+  } else {
+    $button.className = 'nuclear';
+  }
+}
+
+$button.addEventListener('click', handleClick);
